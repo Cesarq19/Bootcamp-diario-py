5ec56b9d79f43065d4cf9ed63d8ff020cdf232d5
Cesarq19 <cepaquin@espol.edu.ec>
2022-12-27 22:18:07 -0500

    Se añade el reto del dia 8 y sus funciones

---
+++ Day8-CaesarCypher.py
@@ -0,0 +1,45 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+# Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs
+
+def encrypt(text,shift):
+    encode_text = ""
+    for letter in text:
+        if not letter in alphabet:
+            encode_text += letter
+        else:
+            index = alphabet.index(letter) + shift
+            if index > 25:
+                index = index - 26
+            encode_text += alphabet[index]
+    print(f"The encoded text is {encode_text}")
+
+#Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
+
+def decrypt(text,shift):
+    decode_text = ""
+    for letter in text:
+        if not letter in alphabet:
+            decode_text += letter
+        else:
+            index = alphabet.index(letter) - shift
+            if index < 0:
+                index = index + 26
+            decode_text += alphabet[index]
+    print(f"The decoded text is {decode_text}")
+
+programa = True
+while programa:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if direction in ['encode','decode']:
+        programa = False
+        print('Ingrese una direccion valida')
+
+if(direction=="encode"):
+    encrypt(text=text,shift=shift)
+else:
+    decrypt(text=text,shift=shift)
+
