dbc8d9eb703e5fe4dfa873a748d29f81b1c83cac
Cesarq19 <cepaquin@espol.edu.ec>
2022-12-29 20:55:56 -0500

    Reto completado Day 9

--- Day9-SecretAuction.py
+++ Day9-SecretAuction.py
@@ -1,3 +1,6 @@
+import os
+
+
 logo = '''
                          ___________
                          \         /
@@ -12,4 +15,19 @@ logo = '''
                       /_______________\\
 '''
 print(logo)
+bids = dict()
 print("Welcome to the secret auction program.")
+program = True
+while program:
+  name = input("What is your name?: ")
+  bid = int(input("What's your bid? : $"))
+  more_bidder = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+  bids[name] = bid
+  if(more_bidder=="yes"):
+    os.system("cls")
+  else:
+    os.system("cls")
+    program = False
+
+winner_name = max(bids,key=bids.get)
+print(f"The winner is {winner_name} with a bid of ${bids[winner_name]}")
